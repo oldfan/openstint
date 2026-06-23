655c455ac4257e7102798bb58ec48cb722efd444
oldfan <oldfan1@gmail.com>
2026-06-23 16:16:18 +0800

    Refactor transponder type to use TransponderSystem

--- src/passing.hpp
+++ src/passing.hpp
@@ -5,6 +5,7 @@
 #include <mutex>
 #include <map>
 #include <deque>
+#include <string>
 #include <vector>
 #include <utility>
 
@@ -12,38 +13,45 @@
 #include "frame.hpp"
 
 
+enum class TransponderSystem {
+    OpenStint,  // openstint transponder
+    AMB         // rc3 and rc4 transponders
+};
+
+std::string transponder_system_name(TransponderSystem tsys);
+
 struct Detection {
     uint64_t timestamp;
+    uint64_t timecode;
     float rssi;
-    float evm;
 
-    Detection(uint64_t _ts, float _rssi, float _evm) : timestamp(_ts), rssi(_rssi), evm(_evm) {};
+    Detection(uint64_t _ts, uint64_t _tc, float _rssi) : timestamp(_ts), timecode(_tc), rssi(_rssi) {};
 };
 
 struct TimeSyncMsg {
-    uint64_t timestamp;
+    uint64_t decoder_timestamp;
     uint32_t transponder_timestamp;
 
-    TimeSyncMsg(uint64_t _ts, uint64_t _transponder_ts) : timestamp(_ts), transponder_timestamp(_transponder_ts) {};
+    TimeSyncMsg(uint64_t _ts, uint64_t _transponder_ts) : decoder_timestamp(_ts), transponder_timestamp(_transponder_ts) {};
 };
 
 struct Passing {
     uint64_t timestamp;
-    TransponderType transponder_type;
+    TransponderSystem transponder_type;
     uint32_t transponder_id;
     float rssi;
     size_t hits;
-    uint32_t duration;
+    uint64_t duration;
 };
 
 struct TimeSync {
     uint64_t timestamp;
-    TransponderType transponder_type;
+    TransponderSystem transponder_type;
     uint32_t transponder_id;
     uint32_t transponder_timestamp;
 };
 
-typedef std::pair<TransponderType, uint32_t> TransponderKey;
+typedef std::pair<TransponderSystem, uint32_t> TransponderKey;
 
 class PassingDetector {
     std::map<TransponderKey, std::deque<Detection>> detections;
@@ -55,4 +63,5 @@ public:
     void timesync(const Frame* frame, uint32_t transponder_timestamp);
     std::vector<TimeSync> identify_timesyncs(uint64_t margin);
     std::vector<Passing> identify_passings(uint64_t deadline);
+    std::vector<uint32_t> passings_between(TransponderSystem tsys, uint64_t timestamp_from, uint64_t timestamp_until);
 };
